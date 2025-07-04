00ab7948bf7a55fd78af39f631b56fb6aad89658
giter2222 <152691844+giter2222@users.noreply.github.com>
2025-07-04 20:54:27 +0100

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,31 +1,23 @@
-from django.urls import path, re_path
-from . import views
+"""
+URL configuration for hotel_management project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path , include
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('chambres/', views.chambre_list, name='chambre_list'),
-    path('chambres/new/', views.chambre_create, name='chambre_create'),
-    path('chambres/<int:pk>/edit/', views.chambre_update, name='chambre_update'),
-    path('chambres/<int:pk>/delete/', views.chambre_delete, name='chambre_delete'),
-    path('register/', views.register_client, name='register_client'),
-    path('login/', views.client_login, name='client_login'),
-    path('account/', views.client_account, name='client_account'),
-    path('logout/', views.client_logout, name='client_logout'),
-    path('reserver/<int:chambre_id>/', views.reserver_chambre, name='reserver_chambre'),
-    path('dashboard/', views.dashboard_view, name='dashboard'),
-    path('dashboard/data/', views.dashboard_data, name='dashboard_data'),
-    path('facture/create/<int:reservation_id>/', views.facture_create, name='facture_create'),
-    path('reservation/<int:reservation_id>/pdf/', views.generate_reservation_pdf, name='generate_reservation_pdf'),
-    re_path(r'^delete_reservation/(?P<reservation_id>\d+)/$', views.delete_reservation, name='delete_reservation'),
-    path('recherche/', views.rechercher_chambre, name='recherche'),
-    # Admin URLs
-    path('admin/login/', views.admin_login, name='admin_login'),
-    path('admin/dashboard/', views.admin_dashboard, name='admin_dashboard'),
-    path('admin/chambre/add/', views.admin_add_chambre, name='admin_add_chambre'),
-    path('admin/chambre/<int:chambre_id>/edit/', views.admin_edit_chambre, name='admin_edit_chambre'),
-    path('admin/chambre/<int:chambre_id>/delete/', views.admin_delete_chambre, name='admin_delete_chambre'),
-    path('admin/logout/', views.admin_logout, name='admin_logout'),
-
-    # Vous pouvez ajouter de la même manière pour Clients et Réservations
-    path('chambres/', views.chambres_disponibles, name='chambres_disponibles'),
+    path('', include('hotel.urls')),
+    path('admin/', admin.site.urls),
 ]
